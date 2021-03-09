5f79e5f1eee1644094844bd1d0b07f159753fefe
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-03-09 10:34:48 -0500

    commit tree 454576b5f1bdf195895a5f1bf4000ec7704550f3

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,11 @@
+require 'pry'
 class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find_by(id: params[:id])
+  end 
+
 end
